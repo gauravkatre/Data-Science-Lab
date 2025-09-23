4a296fb92269aeb7183da4318104e294533c7640
Gaurav Katre <130164324+gauravkatre@users.noreply.github.com>
2025-09-23 09:59:41 +0530

    Update pract8.R

--- pract8.R
+++ pract8.R
@@ -6,36 +6,34 @@ pets <- data.frame(
   Size = c("medium", "small", "medium", "large", "small", "extra small", "large"),
   Weight = c(21, 8, 4, 6, 7, 2, 36),
   Breed = c("dog", "dog", "cat", "cat", "dog", "cat", "dog"),
-  stringsAsFactors = TRUE   # store character variables as factors
+  stringsAsFactors = TRUE 
 )
 print(pets)
 
 
-# a) Invert the values for Breed
 
-levels(pets$Breed) <- c("cat", "dog")   # Swapping levels
+
+levels(pets$Breed) <- c("cat", "dog")
 print(pets$Breed)
 
 
-# b) Edit levels of Breed variable
-# Already done above (Breed is factor with swapped labels)
 
-# c) Access a specific variable
-print(pets$Weight)        # Access Weight column
-print(pets[, "Size"])     # Access Size column
 
-# d) Add a new column Age_in_years
+print(pets$Weight)      
+print(pets[, "Size"])    
+
+
 
 pets$Age_in_years <- pets$Months_old / 12
 print(pets)
-# e) Frequency table of Breed vs Size
+
 print(table(pets$Breed, pets$Size))
 
-# f) Average Weight grouped by Breed
+
 print(aggregate(Weight ~ Breed, data = pets, FUN = mean))
 
-# g) Boxplot of Weight by Breed
 boxplot(Weight ~ Breed, data = pets,
         col = c("lightblue", "lightgreen"),
         main = "Weight Differences Between Breeds",
         xlab = "Breed", ylab = "Weight")
+
